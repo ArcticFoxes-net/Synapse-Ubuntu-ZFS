b1658d3bad03accc609813b0e295f64918b23029
Tommy <contact@tommytran.io>
2023-09-07 19:47:56 -0700

    Curve secp384r1
    Signed-off-by: Tommy <contact@tommytran.io>

--- certbot.sh
+++ certbot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-certbot certonly -d matrix.arcticfoxes.net -d element.arcticfoxes.net -d invite.arcticfoxes.net --key-type ecdsa --reuse-key --must-staple --deploy-hook "certbot-ocsp-fetcher -o /var/cache/certbot-ocsp-fetcher"
+certbot certonly -d matrix.arcticfoxes.net -d element.arcticfoxes.net -d invite.arcticfoxes.net --key-type ecdsa --reuse-key --must-staple --elliptic-curve secp384r1 --deploy-hook "certbot-ocsp-fetcher -o /var/cache/certbot-ocsp-fetcher"
